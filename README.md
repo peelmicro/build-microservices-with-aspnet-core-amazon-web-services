7114d48131ee63adca8e3e82e4203585182d6910
Juan Pablo Perez <Juan.Pablo.Perez@retailinmotion.com>
2019-05-18 21:27:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Build Microservices with .NET Core & Amazon Web Services
+
+> Source code for the `Build Microservices with .NET Core & Amazon Web Services` Udemy course
+
+## Create the `appsettings.json` document from the `Removeme.appsettings.json` document
+
+- Copy the `Removeme.appsettings.json` to `appsettings.json` and put your own credentials
+
+## Follow the course
+
+Follow the course on https://www.udemy.com/build-microservices-with-aspnet-core-amazon-web-services/
---
+++ WebAdvert.Web/Controllers/AccountsController.cs
@@ -0,0 +1,52 @@
+﻿using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Amazon.Runtime.Internal.Transform;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebAdvert.Web.Models.Accounts;
+
+namespace WebAdvert.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+
+        public AccountsController(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager, CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _pool = pool;
+        }
+
+        public IActionResult Signup()
+        {
+            var model = new SignupModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+                if (user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email already exists");
+                    return View(model);
+                }
+                user.Attributes.Add(CognitoAttribute.Name.ToString(), model.Email);
+                var createdUser = await _userManager.CreateAsync(user, model.Password);
+                if (createdUser.Succeeded)
+                {
+                    RedirectToAction("Confirm");
+                }
+                
+            }
+            return View();
+        }
+    }
+}
